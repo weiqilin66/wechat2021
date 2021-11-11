7fe447c7f649c2125b2ea3ea1c2b24d104cfb8c8
linwq1 <linwq1@yusys.com.cn>
2021-11-11 17:35:52 +0800

    1
    Change-Id: Id2cdf4a46dddbc05c226ec199c1c2ad6b4407b4c

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,5 +1,5 @@
 // 引入函数
-import {request} from "../../request/index.js"
+import { request } from "../../request/index.js"
 
 Page({
 
@@ -7,42 +7,43 @@ Page({
    * 页面的初始数据
    */
   data: {
-    swiperList:[],
-
+    swiperList: [],
+    cateList:[],
     //模拟返回数据
-    resp : {
+    resp: {
+
       "message": [
-          {
-              "image_src": "https://api-hmugo-web.itheima.net/pyg/banner1.png",
-              "open_type": "navigate",
-              "goods_id": 129,
-              "navigator_url": "/pages/goods_detail/index?goods_id=129"
-          },
-          {
-            "image_src": "https://api-hmugo-web.itheima.net/pyg/banner3.png",
-            "open_type": "navigate",
-            "goods_id": 121,
-            "navigator_url": "/pages/goods_detail/index?goods_id=121"
+        {
+          "image_src": "https://api-hmugo-web.itheima.net/pyg/banner1.png",
+          "open_type": "navigate",
+          "goods_id": 129,
+          "navigator_url": "/pages/goods_detail/index?goods_id=129"
+        },
+        {
+          "image_src": "https://api-hmugo-web.itheima.net/pyg/banner3.png",
+          "open_type": "navigate",
+          "goods_id": 121,
+          "navigator_url": "/pages/goods_detail/index?goods_id=121"
         },
         {
           "image_src": "https://api-hmugo-web.itheima.net/pyg/banner2.png",
           "open_type": "navigate",
           "goods_id": 122,
           "navigator_url": "/pages/goods_detail/index?goods_id=122"
-      }
+        }
       ],
       "meta": {
-          "msg": "获取成功",
-          "status": 200
+        "msg": "获取成功",
+        "status": 200
       }
-  }
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+
     // 原生发送异步请求 生产环境需要配置白名单域名 ,开发环境详情配置中修改不校验
     // wx.request({
     //   url: 'https://www.baidu.com',      
@@ -55,62 +56,77 @@ Page({
     //     })
     //   }     
     // })
-    // 封装后请求
+    this.getSwiperList();
+    this.getCateList();
+  },
+  /*获取轮播图 */
+  getSwiperList() {
+    // 封装后请求 防止原生请求的回调地狱
     request({
-      url: 'https://www.baidu.com'      
-    }).then(result =>{
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+    }).then(result => {
       this.setData({
-        swiperList:this.data.resp.message
+        swiperList: result.data.message
       })
     })//后续请求继续then
   },
-
+  /*获取导航栏数据 */
+  getCateList() {
+    // 封装后请求 防止原生请求的回调地狱
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'
+    }).then(result => {
+      this.setData({
+        cateList: result.data.message
+      })
+    })//后续请求继续then
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
+
   }
 })
